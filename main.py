d5218b4ba39e059219da90b0c80a2434b39335d2
czimm79git <czimmermann@mines.edu>
2019-07-29 13:22:24 -0600

    set default thread values based on params, push param changes to thread

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import pyqtSlot
 import sys, os
 from threads.DataGenerator import Generator
-from parametertree import MyParamTree
+from ParameterTree import MyParamTree
 
 class MyWindow(QtGui.QMainWindow):
     ''' Setting up the main window. Anything with 'self' refers to the main window.'''
     def __init__(self):
         super().__init__() # Inherit everything from QWidget
+        self.ptr = 1
         self.curve1 = np.sin(np.arange(0,100,step=0.1)) # placeholder toggle curve
         self.curvecolors = ['b','g','r','c','y','m']
-        self.readThread = Generator() # Instantiate my thread
-        self.readThread.newData.connect(self.on_new_data_update_plot) # Connect the data from the thread to my method
         self.set_style()
         self.initUI()
+
+        self.readThread = Generator(
+            multi = self.t.getParamValue('Current Multiplier'),
+            freq = self.t.getParamValue('Frequency')) # Instantiate my thread with default values
+
+        self.readThread.newData.connect(self.on_new_data_update_plot) # Connect the data from the thread to my method
+
 
     def set_style(self):
         ''' Simply set some config options and themes. '''
@@ -45,42 +51,65 @@ class MyWindow(QtGui.QMainWindow):
         self.btn = QtWidgets.QPushButton('Toggle a stupid sin wave.')
         self.btn.clicked.connect(self.on_button_clicked) # Connect it to my method
 
-        #   Checkbox
-        self.check = QtWidgets.QCheckBox('A Fucking Checkbox that raves')
-        self.check.stateChanged.connect(self.call_data_gen_while_check) # Connect it to my method
 
         # Parameter Tree
         self.t = MyParamTree()
-
+        self.t.paramChange.connect(self.change)
         # Add widgets to the layout in their proper positions
         layout.addWidget(self.t, 0, 0, 1, 1) # row, col, rowspan, colspan
         layout.addWidget(self.p1, 1, 0)
         layout.addWidget(self.btn, 2, 0)
-        layout.addWidget(self.check, 0, 1)
+
 
 
     def on_button_clicked(self):
         ''' When the button is clicked, toggle plotting self.curve1 '''
         if self.ptr:
-            self.p1.plot(self.curve1, pen='k')
+            self.readThread.multi += 10
         else:
-            self.p1.clear()
+            pass
+            #self.p1.clear()
         self.ptr = not self.ptr
 
 
     def on_new_data_update_plot(self, incomingData):
         ''' Each time the thread sends data, plot every row as a new line.'''
-        self.p1.clear()
+        self.p1.clear() # Clear last update's lines
         for i in range(0,np.shape(incomingData)[0]): # For each row in incomingData
             # Plot all rows
             self.p1.plot(incomingData[i], clear=False, pen=self.curvecolors[i])
 
 
-    def call_data_gen_while_check(self, int):
-        ''' Only run the thread when the checkbox is checked.'''
-        if self.check.isChecked():
+    def change(self, param, changes):
+        '''Every time a change is made in the parameter tree, it comes here to be processed.'''
+        print("tree changes: ")
+        for param, change, data in changes:
+            path = self.t.p.childPath(param)
+            if path is not None:
+                childName = '.'.join(path)
+            else:
+                childName = param.name()
+            print(f'  path:  {path}')
+            print(self.t.getParamValue('Toggle'))
+            print(f'  parameter: {childName}')
+            print(f'  change:    {change}')
+            print(f'  data:      {str(data)}')
+            print('  ----------')
+
+            # Logic for sending changes to readThread (writeThread later)
+            if path[1] == 'Toggle':
+                self.toggle_readThread(data)
+            if path[1] == 'Current Multiplier':
+                self.readThread.multi = data
+            if path[1] == 'Frequency':
+                self.readThread.freq = data
+
+
+    def toggle_readThread(self, data):
+        '''When a checkbox is changed, it starts or stops the thread.'''
+        if data == True:
             self.readThread.start()
-        else:
+        elif data == False:
             self.readThread.running = False
             self.p1.clear()
 
