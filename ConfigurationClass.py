80be5c87ed4e82a0492ef8aa2c201258f70f8076
czimm79git <czimmermann@mines.edu>
2019-08-09 12:33:46 -0600

    added config.ini file and updated Configuration to pull values from it

--- ConfigurationClass.py
+++ ConfigurationClass.py
@@ -1,26 +1,38 @@
-class Configuration():
-    ''' Contains all of the *mostly* static configurable constants. '''
-    def __init__(self):
-        # Reading
-        self.daq_name = 'Dev1'
-        self.readchannel_list = ['ai0','ai1','ai2','ai3','ai4','ai5']
-        self.daq_rate = 1000 # Rate at which the DAQ collects data in samples per second
-        self.chunksize = 100 # The reader waits until there are [chunksize] data points and pushes them to the plot
+import configparser
+import ast
 
-        # Writing
-        self.funcg_name = 'cDAQ1Mod1'
-        self.writechannel_list = [0, 1, 2]
-        self.funcg_rate = 8000
-        self.writechunksize = self.funcg_rate // 10
-        # ParamTree
+class Configuration:
+    """ Reads in all of the configurable constants from config.ini. """
+    def __init__(self):
+        inifile = configparser.ConfigParser()  # Initialize the configparser
+        inifile.read('config.ini')  # read in the config.ini file
+
+        # READ
+        self.daq_name = ast.literal_eval(inifile['READ']['daq_name'])
+        self.readchannel_list = ast.literal_eval(inifile['READ']['readchannel_list'])
+        self.daq_rate = int(inifile['READ']['daq_rate'])
+        self.readchunksize = int(inifile['READ']['readchunksize'])
+
+        # WRITE
+        self.funcg_name = ast.literal_eval(inifile['WRITE']['funcg_name'])
+        self.writechannel_list = ast.literal_eval(inifile['WRITE']['writechannel_list'])
+        self.funcg_rate = int(inifile['WRITE']['funcg_rate'])
+        self.writechunksize = self.funcg_rate // 10  # This keeps the frequency of the wave uniform.
+
+        # DEFAULT SIGNAL VALUES
         self.defaults = {
-            'vmulti' : 1.0,
-            'freq' : 10,
-            'camber' : 0,
-            'zphase' : 0,
-            'xvoltamp' : 1.0,
-            'yvoltamp' : 1.0,
-            'zvoltamp' : 1.0,
+            'vmulti': float(inifile['DEFAULT SIGNAL VALUES']['vmulti']),
+            'freq': int(inifile['DEFAULT SIGNAL VALUES']['freq']),
+            'camber': int(inifile['DEFAULT SIGNAL VALUES']['camber']),
+            'zphase': int(inifile['DEFAULT SIGNAL VALUES']['zphase']),
+            'calib_xamp': float(inifile['DEFAULT SIGNAL VALUES']['calib_xamp']),
+            'calib_yamp': float(inifile['DEFAULT SIGNAL VALUES']['calib_yamp']),
+            'calib_zamp': float(inifile['DEFAULT SIGNAL VALUES']['calib_zamp']),
             }
 
-        self.zcoeff = 0.653
+        self.zcoeff = float(inifile['DEFAULT SIGNAL VALUES']['zcoeff'])
+
+
+if __name__ == '__main__':
+    config = Configuration()
+    # var = config.daq_name
